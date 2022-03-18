8fd78641e4e6df0180c243ec488f34e016015f66
GobindSetia <gobind.setia@gmail.com>
2022-03-18 20:01:24 +0530

    added projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,92 @@
 import "./loader.css";
 export const About = () => {
-    return (
-      <section id="about">
-        <div className="sectionHeading">
-          <h1>About me</h1>
-        </div>
-        <div id="introText">
-            <p>Hello there</p>
-            <p>I am Gobind Setia, a graduate in B.Tech.(CSE) from Amity University Noida. I have worked as an SEO executive for an year. Now I am a student of Full Stack Web Developer at Masai School.</p>
-        </div>
-        <div id="techStack">
-        <div id="loader">
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/html-5--v1.png"/></span>
-        <span><img align="left" alt="CSS" src="https://img.icons8.com/color/48/000000/css3.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/javascript--v1.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/external-others-amoghdesign/48/000000/external-react-native-soleicons-fill-vol-1-others-amoghdesign.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/redux.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/nodejs.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/mongodb.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/git.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/npm.png"/></span>
-        <span><img align="left" src="https://img.icons8.com/color/48/000000/bootstrap.png"/></span>
+  return (
+    <section id="about">
+      <div className="sectionHeading">
+        <h1>About me</h1>
       </div>
+      <div id="introText">
+        <p>Hello there</p>
+        <p>
+          I am Gobind Setia, a graduate in B.Tech.(CSE) from Amity University
+          Noida. I have worked as an SEO executive for an year. Now I am a
+          student of Full Stack Web Developer at Masai School.
+        </p>
+      </div>
+      <div id="techStack">
+        <div id="loader">
+          <span>
+            <img
+              align="left"
+              alt="HTML"
+              src="https://img.icons8.com/color/48/000000/html-5--v1.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="CSS"
+              src="https://img.icons8.com/color/48/000000/css3.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="JavaScript"
+              src="https://img.icons8.com/color/48/000000/javascript--v1.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="Git"
+              src="https://img.icons8.com/color/48/000000/bootstrap.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="ReactJS"
+              src="https://img.icons8.com/external-others-amoghdesign/48/000000/external-react-native-soleicons-fill-vol-1-others-amoghdesign.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="Redux"
+              src="https://img.icons8.com/color/48/000000/redux.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="NodeJS"
+              src="https://img.icons8.com/color/48/000000/nodejs.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="MongoDB"
+              src="https://img.icons8.com/color/48/000000/mongodb.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="Git"
+              src="https://img.icons8.com/color/48/000000/git.png"
+            />
+          </span>
+          <span>
+            <img
+              align="left"
+              alt="NPM"
+              src="https://img.icons8.com/color/48/000000/npm.png"
+            />
+          </span>
         </div>
-      </section>
-    );
-  };
+      </div>
+    </section>
+  );
+};
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,39 @@
-
+import CloudDoneIcon from '@material-ui/icons/CloudDone';
+import GitHubIcon from '@material-ui/icons/GitHub';
 export const Projects = () => {
     return (
       <section id="projects">
+        <div className="sectionHeading">
+        <h1>Projects</h1>
+        </div>
+        
         <div id="projectsDiv">
             <div className="project">
-              .projectImage
+              <div className="projectImage" id="travelocityImage"></div>
+              <div className="projectDetail">
+                <h4>Clone of Travelocity.com</h4>
+                <p>Flight booking functionality of travelocity.com. Allows selection of To-From cities and travel date. Fetched flights can be sorted based on the price.</p>
+                <p>HTML | CSS | JS</p>
+                <div>
+                  <button><CloudDoneIcon/><br/>Deployed Link</button>
+                  <button><GitHubIcon/><br/>GitHub Repository</button>  
+                </div> 
+              </div>
             </div>
+
+            <div className="project">
+              <div className="projectImage" id="vijaysalesImage"></div>
+              <div className="projectDetail">
+                <h4>Clone of VijaySales.com</h4>
+                <p>Mobile ordering functionality of vijaysales.com. Uses NodeJs backend server to fetch products to be displayed on index-page.</p>
+                <p>HTML | CSS | JS | Express | MongoDB</p>
+                <div>
+                  <button><CloudDoneIcon/><br/>Deployed Link</button>
+                  <button><GitHubIcon/><br/>GitHub Repository</button>             
+                </div>
+              </div>
+            </div>
+
         </div>
       </section>
     );
