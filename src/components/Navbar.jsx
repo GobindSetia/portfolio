14f4720f340bfa5ed9050d064328c50fa01e02ac
Gobind Setia <94806240+GobindSetia@users.noreply.github.com>
2022-03-24 18:24:54 +0530

    Removing down arrow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
         
 
         <div id="navbarRight">
-        <div id="resumeDownload"><button onClick={()=>{window.open("https://drive.google.com/file/d/1YvSOSCy1api8yRDmKo-6o4SxEnDOkhc3/view", "_blank")}}>Resume  ▼</button></div>
+        <div id="resumeDownload"><button onClick={()=>{window.open("https://drive.google.com/file/d/1YvSOSCy1api8yRDmKo-6o4SxEnDOkhc3/view", "_blank")}}>Resume</button></div>
           <a href="#about">About</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
