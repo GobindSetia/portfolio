7293a708ab101ca01dd8a56fad6e5a5a02d2a321
GobindSetia <gobind.setia@gmail.com>
2022-03-29 18:08:48 +0530

    form working on netlify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,38 +7,77 @@ export const Contact = () => {
         <h1>Contact Me</h1>
       </div>
       <div className="loader" id="contactLinks">
-        <span><a href="https://www.linkedin.com/in/gobindsetia/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt=""/></a></span>
-        <span><a href="https://github.com/GobindSetia" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt=""/></a></span>
-        <span><a href="https://twitter.com/GobindSetia" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt=""/></a></span>
-        <span><a href="https://join.skype.com/invite/uhe7dMrbvdgC" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/color/48/000000/skype--v1.png" alt=""/></a></span>
-
+        <span>
+          <a
+            href="https://www.linkedin.com/in/gobindsetia/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/color/48/000000/linkedin.png"
+              alt=""
+            />
+          </a>
+        </span>
+        <span>
+          <a
+            href="https://github.com/GobindSetia"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/ios-glyphs/48/000000/github.png"
+              alt=""
+            />
+          </a>
+        </span>
+        <span>
+          <a
+            href="https://twitter.com/GobindSetia"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/color/48/000000/twitter--v1.png"
+              alt=""
+            />
+          </a>
+        </span>
+        <span>
+          <a
+            href="https://join.skype.com/invite/uhe7dMrbvdgC"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://img.icons8.com/color/48/000000/skype--v1.png"
+              alt=""
+            />
+          </a>
+        </span>
       </div>
-      <div id="contactInfo">
+      <div id="infoAndForm">
+        <div id="contactInfo">
           <p>Address: Fatehabad, Haryana(125051)</p>
           <p>Email: gobind.setia@gmail.com</p>
           <p>Contact: +917056403400</p>
-      </div>
-      <form method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" />
-            </label>
-          </p>
-          <p>
+        </div>
+        <div>
+          <form method="post">
+            <input type="hidden" name="form-name" value="contact" />
+
+              <input type="text" name="name" placeholder="Your Name" />
+
+              <input type="email" name="email" placeholder="Your Email" />
+
+              <textarea name="message" placeholder="How can I help you?"/>
+        
+            
+
             <button type="submit">Send</button>
-          </p>
-        </form>
+          </form>
+        </div>
+      </div>
     </section>
   );
 };
